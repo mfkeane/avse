29aa00b1c937d11ed0b07a2f8ecc8c84adb8277d
Michelle <shellyk1997@gmail.com>
2020-04-18 20:34:12 +1000

    imports msg from avse_common
    rosrun avse gpslistener.py from workspace root

--- avse/src/avse_playground/gpslistener.py
+++ avse/src/avse_playground/gpslistener.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-#from agpsimu_common.msg import NavSatFix
+from avse_common.msg import NavSatFix
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 def listener():
     rospy.init_node('gps_listener', anonymous=True)
-    rospy.Subscriber("/android/fix", String, callback)
+    rospy.Subscriber("/android/fix", NavSatFix, callback)
     rospy.spin()
 
 if __name__ == '__main__':
