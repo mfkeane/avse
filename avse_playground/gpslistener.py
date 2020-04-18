a9877af345c7e9ba911e2c86c22d5da4abe7db43
Michelle <shellyk1997@gmail.com>
2020-04-18 17:41:30 +1000

    gpslistener.py subscribes to /android/fix

---
+++ avse_playground/gpslistener.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+#from agpsimu_common.msg import NavSatFix
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def listener():
+    rospy.init_node('gps_listener', anonymous=True)
+    rospy.Subscriber("/android/fix", String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
