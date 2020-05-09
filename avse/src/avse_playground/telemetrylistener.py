abf7dd4bebd95979bb6ca45c75e68f78fd47c8cb
Michelle <shellyk1997@gmail.com>
2020-05-09 15:52:13 +1000

    GPS raw only plot works

--- avse/src/avse_playground/telemetrylistener.py
+++ avse/src/avse_playground/telemetrylistener.py
@@ -68,34 +68,36 @@ class Server:
         mx = np.cumsum(self.dx)
 	my = np.cumsum(self.dy)
 
-        fig = plt.figure(figsize=(16,9))
-
-        # EKF State
-        plt.quiver(x0,x1,np.cos(x2), np.sin(x2), color='#94C600',
-               units='xy', width=0.05, scale=0.5)
-        plt.plot(x0,x1, label='Position', c='k', lw=5)
-
-        # Measurements
-        plt.scatter(mx[::5],my[::5], s=50, label='GPS Measurements', marker='+')
-        #cbar=plt.colorbar(ticks=np.arange(20))
-        #cbar.ax.set_ylabel(u'EPE', rotation=270)
-        #cbar.ax.set_xlabel(u'm')
-
         # Start/Goal
-	if len(x0)>2:
-            plt.scatter(x0[0],x1[0], s=60, label='Start', c='g')
-            plt.scatter(x0[-1],x1[-1], s=60, label='Goal', c='r')
+	if len(x0)==1:
+            fig = plt.figure(figsize=(16,9))
+            # EKF State
+       	    #plt.quiver(x0,x1,np.cos(x2), np.sin(x2), color='#94C600',units='xy', width=0.05, scale=0.5)
+            #plt.plot(x0,x1, label='Position', c='k', lw=5)
+
+            # Measurements
+            #plt.scatter(mx[::5],my[::5], s=50, label='GPS Measurements', marker='+')
+            #cbar=plt.colorbar(ticks=np.arange(20))
+            #cbar.ax.set_ylabel(u'EPE', rotation=270)
+            #cbar.ax.set_xlabel(u'm')	
+            
+            #plt.scatter(x0[0],x1[0], s=60, label='Start', c='g')
+            #plt.scatter(x0[-1],x1[-1], s=60, label='Goal', c='r')
 
             plt.xlabel('X [m]')
             plt.ylabel('Y [m]')
             plt.title('Position')
-            plt.legend(loc='best')
-            plt.axis('equal')
+            #plt.legend(loc='best')
+            #plt.axis('equal')
             #plt.tight_layout()
 
-            plt.savefig('Basic-GPS-Position.png',
-                dpi=72, transparent=True, bbox_inches='tight')
-	
+        
+	if len(x0)>0:
+            plt.plot(x0[-1],x1[-1],'*')
+            plt.axis('equal')
+            plt.draw()
+            plt.pause(0.000000001)
+	    plt.savefig('Basic-GPS-Position.png', dpi=72, transparent=True, bbox_inches='tight')
 
     def mf_callback(self,mf):
         self.mf = mf
@@ -115,8 +117,10 @@ class Server:
         if self.past_gps is not None:
              present = utm.from_latlon(gps.latitude,gps.longitude)
              past = utm.from_latlon(self.past_gps.latitude,self.past_gps.longitude)
-             self.dx.append(present[0]-past[0])
-             self.dy.append(present[1]-past[1])
+             dx2 = present[0]-past[0]
+             dy2 = present[1]-past[1]
+             self.dx.append(dx2) #(round(dx2,2))
+             self.dy.append(dy2) #(round(dy2,2))
 	#    self.dx.append(arc * np.cos(gps.latitude*np.pi/180.0) * (gps.longitude-self.past_gps.longitude)) # in m
 	#    self.dy.append(arc * (gps.latitude-self.past_gps.latitude)) # in m
         else:
@@ -124,9 +128,9 @@ class Server:
 	    self.dy.append(0)
 	#self.distance = self.distance + np.sqrt(self.dx[-1]**2+self.dy[-1]**2)
        
-	self.past_position_x.append(self.position.x)
-	self.past_position_y.append(self.position.y)
-	self.position = Vector3(self.past_position_x[-1]+self.dx[-1], self.past_position_y[-1]+self.dy[-1], 0)
+	self.past_position_x.append(self.position.x)#round(self.position.x,2))
+	self.past_position_y.append(self.position.y)#round(self.position.y,2))
+	self.position = Vector3(self.past_position_x[-1]+self.dx[-1],self.past_position_y[-1]+self.dy[-1], 0) #round
 
         self.velocity = Vector3((self.position.x-self.past_position_x[-1])/self.gpsdt, (self.position.y-self.past_position_y[-1])/self.gpsdt, 0)
 
@@ -134,6 +138,30 @@ class Server:
         print("position is: " + str(self.position))
 	print("velocity is: " + str(self.velocity))
 
+	x0=self.past_position_x
+        x1=self.past_position_y
+        x2=self.past_heading
+
+        mx = np.cumsum(self.dx)
+	my = np.cumsum(self.dy)
+
+        # Start/Goal
+	if len(x0)==1:
+            fig = plt.figure(figsize=(16,9))
+        
+            plt.xlabel('X [m]')
+            plt.ylabel('Y [m]')
+            plt.title('Position')
+           
+	if len(x0)>0:
+            plt.plot(x0[-1],x1[-1],'*')
+            plt.axis('equal')
+            plt.draw()
+            plt.pause(0.000000001)
+            plt.plot(x0,x1)
+            #plt.quiver(x0,x1,np.cos(x2), np.sin(x2), color='#94C600',units='xy', width=0.05, scale=0.5)
+#Basic-GPS-Position
+            plt.savefig('Basic-GPS-Position.png', dpi=72, transparent=True, bbox_inches='tight')
 
 if __name__ == '__main__':
     rospy.init_node('listener')
@@ -144,8 +172,8 @@ if __name__ == '__main__':
     #ropsy.Subscriber("/android/imu", Imu, server.imu_callback)
     rospy.Subscriber("/android/magnetometer", MagneticField, server.mf_callback)
 
-    server.telemetry_plot()
-
-
+    #server.telemetry_plot()
+    plt.ion
+    plt.show()
     rospy.spin()
 
