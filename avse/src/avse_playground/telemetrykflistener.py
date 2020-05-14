9051be7d93d7e35ad31bae3e0d27437678435df9
Michelle <shellyk1997@gmail.com>
2020-05-14 15:54:16 +1000

    Issues with accuracy of KF
    Raw data is more accurate, looking into issues with acceleration in x and y having a gravity component

--- avse/src/avse_playground/telemetrykflistener.py
+++ avse/src/avse_playground/telemetrykflistener.py
@@ -58,10 +58,10 @@ class Server:
 	#self.x_next = np.array([[0,0],[0,0]])
         #self.P_next = np.array([[0,0],[0,0]])
 
-        self.kf_past_velocity_x = [0]
-        self.kf_past_velocity_y = [0]
-        self.kf_past_position_x = [0]
-        self.kf_past_position_y = [0]
+        self.kf_past_velocity_x = []
+        self.kf_past_velocity_y = []
+        self.kf_past_position_x = []
+        self.kf_past_position_y = []
         self.P = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
 
         self.time = 0.0
@@ -78,10 +78,39 @@ class Server:
     def kalman(self):
         # Update time rate
         delta_t = self.gpsdt
+	print(delta_t)
 
+	# This section doesn't work as its talking about the change in orientation from the starting orientation I think, so we still get z gravity in the x-y "flat" plane
+        # Convert Quarternion to Yaw, Roll, Pitch
+        # from https://www.kaggle.com/c/career-con-2019/discussion/85303
+        w = self.imu.orientation.w
+        x = self.imu.orientation.x
+        y = self.imu.orientation.y
+        z = self.imu.orientation.z     
+        sinr_cosp = 2 * (w * x + y * z)
+        cosr_cosp = 1 - 2 * (x**2 + y**2)
+        roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+        sinp = 2 * (w * y - z * x)
+        pitch = np.where(np.abs(sinp) >= 1, np.sign(sinp) * np.pi / 2, np.arcsin(sinp))
+
+        siny_cosp = 2 * (w * z + x * y)
+        cosy_cosp = 1 - 2 * (y**2 + z**2)
+        yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+	# Make x-y plane flat in terms of acceleration (no gravity component)
+        R01 = np.array([[math.cos(yaw),-math.sin(yaw),0],[math.sin(yaw),math.cos(yaw),0],[0,0,1]])
+        R12 = np.array([[math.sin(yaw),0,math.cos(yaw)],[0,1,0],[-math.cos(yaw),0,math.sin(yaw)]])
+        R23 = np.array([[1,0,0],[0,math.cos(yaw),-math.sin(yaw)],[0,math.sin(yaw),math.cos(yaw)]])
+        R03 = R01.dot(R12).dot(R23)
+
+        accel0 = np.array([[self.acceleration.x],[self.acceleration.y],[self.acceleration.z]])
+        accel3 = R03.dot(accel0)        
+        print(accel3)
+        
 	# Process
 	xk_1 = self.x_k
-        ak_1 = np.array([[self.acceleration.x], [self.acceleration.y]])
+        ak_1 = np.array([[accel3[0][0]], [accel3[1][0]]])
         A = np.array([[1,0,delta_t,0],[0,1,0,delta_t],[0,0,1,0],[0,0,0,1]])
         B = np.array([[0.5*(delta_t*delta_t),0],[0,0.5*(delta_t*delta_t)],[delta_t,0],[0,delta_t]])
 
@@ -89,9 +118,12 @@ class Server:
         zk = np.array([[self.position.x], [self.position.y]])
         H = np.array([[1,0,0,0],[0,1,0,0]])
 
-        # Variance Matrices
-	Q = np.array([[self.gps.position_covariance[0],0,0,0],[0,self.gps.position_covariance[3],0,0],[0,0,1,0],[0,0,0,1]])
+        # Error Matrices
+        # Disturbance Covariances (model)
+	Q = np.array([[10,0,0,0],[0,10,0,0],[0,0,10,0],[0,0,0,10]])
+        # Noise Covariances (Sensors)
         R = np.array([[1,0],[0,1]])
+                    #[[self.gps.position_covariance[0],0],[0,self.gps.position_covariance[4]]])
 
 	# Prediction Equations
         X_k = A.dot(xk_1) + B.dot(ak_1)
@@ -106,6 +138,7 @@ class Server:
         self.kf_past_position_y.append(self.x_k[1])
         self.kf_past_velocity_x.append(self.x_k[2])
         self.kf_past_velocity_y.append(self.x_k[3])
+        self.kf_position = (self.x_k[0],self.x_k[1],0.0)
 
 	
     def imu_callback(self,imu):
@@ -118,13 +151,23 @@ class Server:
 
     def mf_callback(self,mf):
         self.mf = mf
-	self.past_heading.append(self.heading)        
+	self.past_heading.append(self.heading)
+	#if self.imu is not None:  
+        #    w = self.imu.orientation.w
+        #    x = self.imu.orientation.x
+        #    y = self.imu.orientation.y
+        #    z = self.imu.orientation.z      
+	#    imu_heading = math.tan2(2*(w*z+x*y),1-2*(y*y+z*z))        
 	self.heading = math.atan2(self.mf.magnetic_field.y, self.mf.magnetic_field.x)*(180/math.pi) + self.D
+        #print(self.heading)
+        #print(imu_heading)
         self.past_mf = mf
 
     def gps_callback(self, gps):
         self.gps = gps
         self.gpsdt = gps.header.stamp.to_sec() - self.gpstime
+	if self.gpsdt>5:
+	    self.gpsdt=1.0
         self.gpstime = gps.header.stamp.to_sec()
 
         if self.past_gps is not None:
@@ -151,10 +194,11 @@ class Server:
 
 	self.past_gps = gps
         print("position is: " + str(self.position))
-	print("velocity is: " + str(self.velocity))
+	print("revised position is: " + str(self.kf_position))
 
 	x0=self.past_position_x
         x1=self.past_position_y
+        print(x0,x1)
         x2=self.past_heading
 
         mx = np.cumsum(self.dx)
@@ -170,11 +214,12 @@ class Server:
            
 	if len(x0)>0:
             plt.plot(x0[-1],x1[-1],'*')
+            plt.plot(self.kf_past_position_x, self.kf_past_position_y)
             plt.axis('equal')
             plt.draw()
             plt.pause(0.000000001)
-            plt.plot(x0,x1)
-            plt.plot(self.kf_past_position_x, self.kf_past_position_y)
+            #plt.plot(x0,x1)
+            #plt.plot(self.kf_past_position_x, self.kf_past_position_y)
   
             #Basic-GPS-Position
             plt.savefig('KF-GPS-Position.png', dpi=72, transparent=True, bbox_inches='tight')
